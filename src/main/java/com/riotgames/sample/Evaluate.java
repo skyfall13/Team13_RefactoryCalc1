f1190ee5d84621ffba392a12eb5c888f82c9ab38
OpalCat <noblesse3539@daum.net>
2017-05-14 04:08:30 +0900

    Token 클래스 추가

--- src/main/java/com/riotgames/sample/Evaluate.java
+++ src/main/java/com/riotgames/sample/Evaluate.java
@@ -7,26 +7,26 @@ import java.util.Stack;
  */
 public class Evaluate {
 
-    private String[] postfix;
-    private Stack<String> oStack;
+    private Token token = new Token();
     private Stack<Double> vStack;
 
     public double calc(String[] tokens) {
 
-        this.token2Postfix(tokens);
+        token.token2Postfix(tokens);
 
         return this.evalPostfix();
     }
 
     public double evalPostfix() {
+
         int p = 0;
         String curToken;
         Double popToken1, popToken2, result;
         this.vStack = new Stack<Double>();
 
-        while (p < this.postfix.length && this.postfix[p] != null) {
-            curToken = this.postfix[p++];
-            if (this.isDigit(curToken)) {
+        while (p < this.token.getPostfix().length && this.token.getPostfix()[p] != null) {
+            curToken = this.token.getPostfix()[p++];
+            if (this.token.isDigit(curToken)) {
                 this.vStack.push(Double.parseDouble(String.valueOf(curToken)));
             } else {
                 popToken1 = this.vStack.pop();
@@ -38,103 +38,17 @@ public class Evaluate {
         result = this.vStack.pop();
         return result;
     }
-    public boolean isDigit(String token) {
-        if (token.charAt(0) >= '0' && token.charAt(0) <= '9')
-            return true;
-        else
-            return false;
-    }
 
-    public boolean token2Postfix(String[] args) {
-        int i = 0;
-        int p = 0;
-        String[] tokens = args;
 
-        String curToken, poppedToken, topToken;
-        this.oStack = new Stack<String>();
-        this.postfix = new String[tokens.length];
-
-        while (i < tokens.length) {
-            curToken = tokens[i++];
-            if (this.isDigit(curToken))
-                this.postfix[p++] = curToken;
-            else {
-                if (curToken.charAt(0) == ')') {
-                    if (!this.oStack.isEmpty())
-                        poppedToken = (String) this.oStack.pop();
-                    else
-                        return false;
-
-                    while (poppedToken.charAt(0) != '(') {
-                        this.postfix[p++] = poppedToken;
-                        if (!this.oStack.isEmpty())
-                            poppedToken = (String) this.oStack.pop();
-                        else
-                            return false;
-                    }
-                } else {
-                    int inComingP = inComingPrecedence(curToken);
-                    if (!this.oStack.isEmpty()) {
-                        topToken = (String) this.oStack.peek();
-                        while (inStackPrecedence(topToken) >= inComingP) {
-                            poppedToken = (String) this.oStack.pop();
-                            this.postfix[p++] = poppedToken;
-                            if (!this.oStack.isEmpty())
-                                topToken = (String) this.oStack.peek();
-                            else
-                                break;
-                        }
-                    }
-                    this.oStack.push(curToken);
-                }
-            }
-        }
-        while (!this.oStack.isEmpty()) {
-            poppedToken = (String) this.oStack.pop();
-            this.postfix[p++] = poppedToken;
-        }
-        return true;
-    }
-
-    private int inComingPrecedence(String givenToken) {
-        char tempChar = givenToken.charAt(0);
-
-        switch (tempChar) {
-            case '+':
-            case '-':
-                return 12;
-            case '*':
-            case '/':
-                return 13;
-            case '(':
-                return 20;
-            case ')':
-                return 19;
-            default:
-                return -1;
-        }
-
-    }
-
-    private int inStackPrecedence(String givenToken) {
-
-        char tempChar = givenToken.charAt(0);
-
-        switch (tempChar) {
-            case '+':
-            case '-':
-                return 12;
-            case '*':
-            case '/':
-                return 13;
-            case '(':
-                return 0;
-            case ')':
-                return 19;
-            default:
-                return -1;
-        }
-
-    }
+//    private boolean q1(Stack stack, String object){
+//        if(stack.isEmpty()) {
+//            Object temp = object;
+//            temp = (String) stack.pop();
+//            return true;
+//        }else {
+//            return false;
+//        }
+//    }
 
 }
+
