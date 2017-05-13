8cd74d3c6ebd39342636ad9246e039fca52ec83e
SOO HYUN KIM <skyfall13@naver.com>
2017-05-14 01:18:17 +0900

    Test_ADD

--- src/test/java/com/riotgames/sample/CalcAppTest.java
+++ src/test/java/com/riotgames/sample/CalcAppTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.Matchers.is;
  * Unit test for simple CalcApp.
  */
 public class CalcAppTest {
+
     @Test
     public void testCalcAppDoesCorrectAddOperation() {
         String[] args = new String[] { "1", "+", "2" };
@@ -17,4 +18,50 @@ public class CalcAppTest {
         double result = app.calc(args);
         assertThat(result, is(3.0));
     }
+
+    @Test
+    public void 곱셈_연산_테스트(){
+        String[] args = new String[] {"1", "*" , "2"};
+
+        CalcApp app = new CalcApp();
+        double result = app.calc(args);
+        assertThat(result, is(2.0));
+    }
+
+    @Test
+    public void 뺄셈_연산_테스트(){
+        String[] args = new String[] {"10", "-" , "3"};
+
+        CalcApp app = new CalcApp();
+        double result = app.calc(args);
+        assertThat(result, is(7.0));
+    }
+
+    @Test
+    public void 나눗셈_연산_테스트(){
+        String[] args = new String[] {"1", "/" , "2"};
+
+        CalcApp app = new CalcApp();
+        double result = app.calc(args);
+        assertThat(result, is(0.5));
+    }
+
+    @Test
+    public void 우선순위_연산_테스트(){
+        String[] args = new String[] {"8","+","(","1", "*" , "2",")"};
+
+        CalcApp app = new CalcApp();
+        double result = app.calc(args);
+        assertThat(result, is(10.0));
+    }
+
+    @Test
+    public void 우선순위_연산_테스트_2(){
+        String[] args = new String[] {"2","*","4", "/", "(", "1", "+",  "3.0", ")" ,"-", "3"};
+
+        CalcApp app = new CalcApp();
+        double result = app.calc(args);
+        assertThat(result, is(-1.0));
+    }
+
 }
